d2477520369b0f20c07fe3060155950c36bba945
Samantha Ballard <10902676@live.mercer.edu>
2014-09-04 10:36:47 -0400

    adding exercises and homework

---
+++ exercises/class_5/grade_excersize.rb
@@ -0,0 +1,8 @@
+puts "Please enter five grades."
+grades = [] if grades = array.new
+grade = gets.chomp.to_i
+
+grades.push(grade)
+    average_one = grades[0] + grades[1]
+    average_two = grades[2] + grades[3] + grades [4]
+    puts average_two
