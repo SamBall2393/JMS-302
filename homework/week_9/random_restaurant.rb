a04c52def1d3b37c214182c4ea17ada4d9b91fe5
Samantha Ballard <10902676@live.mercer.edu>
2014-10-21 19:45:56 -0400

    Updated Homework

--- homework/week_9/random_restaurant.rb
+++ homework/week_9/random_restaurant.rb
@@ -1,25 +1,33 @@
-restaurants = [
-    "The Rookery",
-    "Sauced",
-    "Ginger",
-    "Margaritas",
-    "Francars",
-    ]
+#Samantha Ballard and Amanda Barrentine
 
-def random_restaurant restaurants
-    rand_restaurant = rand(restaurants.size)
-    puts "Go to #{ restaurants[rand_restaurant]}."
+restaurants = "restaurant_list.txt"
+
+def random_restaurant restaurants_list
+    rand_restaurant = rand(restaurants_list.size)
+    puts "Go to #{ restaurants_list[rand_restaurant]}."
     return rand_restaurant
 end
 
-puts restaurants
+unless File.exists? restaurants
+    puts "#{restaurants} doesn't exist."
+    exit
+end
+
+restaurants_list = Array.new
+File.open(restaurants, "r").each_line do |line|
+    words = line.chomp.split (/\n/)
+    restaurants_list.push(words)
+end
+
+puts restaurants_list
 puts "Would you like to add to the list of restaurants, yes or no?"
 answer = gets.chomp
 if answer.downcase == "yes"
     puts "What is the name of the new restaurant?"
     restaurant = gets.chomp
-    restaurants.push(restaurant)
-    random_restaurant restaurants
+    restaurants_list.push(restaurant)
+    random_restaurant restaurants_list
 else
-    random_restaurant restaurants
+    random_restaurant restaurants_list
 end
+
